e20b6d4ae19781b2ab8889f1e6740b10ff7426b3
Lavanya Kashyap <Lavanya.Kashyap@kpit.com>
2019-01-13 01:25:09 +0530

    changed the process code

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 
 app.use(express.static(path.join(__dirname , '../public')));
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 
 app.listen(port ,(connected)=>{
